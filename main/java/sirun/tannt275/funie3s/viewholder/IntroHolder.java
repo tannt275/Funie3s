bf66c77d261f4e7146b18aad4c36446b66971e51
tannt275 <tannt275@gmail.com>
2016-04-14 11:03:41 +0700

    modified menuDrawer

---
+++ main/java/sirun/tannt275/funie3s/viewholder/IntroHolder.java
@@ -0,0 +1,14 @@
+package sirun.tannt275.funie3s.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by tannt on 4/13/2016.
+ */
+public class IntroHolder extends RecyclerView.ViewHolder {
+
+    public IntroHolder(View itemView) {
+        super(itemView);
+    }
+}
